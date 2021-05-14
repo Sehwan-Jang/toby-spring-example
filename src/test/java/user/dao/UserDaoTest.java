55914b35f6cb61037c7cf853a819de0aa276f8d2
sehwan-jang <whilter08@gmail.com>
2021-05-14 14:52:55 +0900

    refactor : main 메소드 junit 으로 변경

--- src/test/java/user/dao/UserDaoTest.java
+++ src/test/java/user/dao/UserDaoTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import user.domain.User;
+
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,28 +17,29 @@ public class UserDaoTest {
     void setUp() {
 
     }
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+//        UserDao dao2 = new DaoFactory().userDao();
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
-//    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-////        UserDao dao2 = new DaoFactory().userDao();
-//        UserDao dao = context.getBean("userDao", UserDao.class);
-//
-//        User user = new User();
-//        user.setId("aaron");
-//        user.setName("장세환");
-//        user.setPassword("solo");
-//
-//        dao.delete(user);
-//        dao.add(user);
-//
-//        System.out.println(user.getId() + " 등록 성공");
-//
-//        User user2 = dao.get(user.getId());
-//        System.out.println(user2.getName());
-//        System.out.println(user2.getPassword());
-//
-//        System.out.println(user2.getId() + " 조회 성공");
-//    }
+        User user = new User();
+        user.setId("aaron");
+        user.setName("장세환");
+        user.setPassword("solo");
+
+        dao.delete(user);
+
+        assertThat(dao.getCount()).isEqualTo(0);
+
+        dao.add(user);
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user.getName()).isEqualTo(user2.getName());
+        assertThat(user.getPassword()).isEqualTo(user2.getPassword());
+        assertThat(dao.getCount()).isEqualTo(1);
+    }
 
     @Test
     void beanTest() {
